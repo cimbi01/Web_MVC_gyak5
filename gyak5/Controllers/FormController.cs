e0b6bcb306cd639a9cfd397949d12e1e0abee853
Cimbi <Cimbi@DESKTOP-0OTU7V4>
2019-10-23 19:28:28 +0200

    Input kész

---
+++ gyak5/Controllers/FormController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace gyak5.Controllers
+{
+    public class FormController : Controller
+    {
+        public ActionResult FormHelper()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormHelper(Models.Diak d)
+        {
+            return View(d);
+        }
+        public ActionResult FormHtml()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult FormHtml(Models.Diak d)
+        {
+            return View(d);
+        }
+    }
+}
---
+++ gyak5/Models/Diak.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gyak5.Models
+{
+    public class Diak
+    {
+        public string Nev { get; set; }
+        public int PontSzam { get; set; }
+    }
+}
